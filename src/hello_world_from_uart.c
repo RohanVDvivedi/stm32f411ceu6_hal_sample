0e9a2451438ff0ee3393febcc0733a7bda79b53d
RohanVDvivedi <rohandvivedi@gmail.com>
2026-01-20 01:01:38 +0530

    binaries built successfully

--- src/hello_world_from_uart.c
+++ src/hello_world_from_uart.c
@@ -1,13 +1,23 @@
 #include<hello_world_from_uart.h>
 
+#include<capp/capp.h>
+
+#include<string.h>
+
 void print_hello_world_from_uart(UART_HandleTypeDef* huart)
 {
-	const char msg[] = "Hello, world!\r\n";
+	static int i = 0;
+	static char msg[] = "Hello, world!";
+
+	temp t;
+	init_temp(&t, i, msg);
+	char buffer[100];
+	sprintf(buffer, "%s, %d\r\n", t.str, t.s);
 
 	HAL_UART_Transmit(
 		huart,                  /* UART handle */
-		(uint8_t *)msg,          /* data buffer */
-		sizeof(msg) - 1,         /* length (exclude '\0') */
-		HAL_MAX_DELAY            /* blocking timeout */
+		(uint8_t *)buffer,      /* data buffer */
+		strlen(buffer),         /* length (exclude '\0') */
+		HAL_MAX_DELAY           /* blocking timeout */
 	);
 }
